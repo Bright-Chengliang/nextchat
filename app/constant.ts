33ddb092a6e43cdb5d5061d1c5c8eeb3f13abbf3
Bright-Chengliang <962430566@qq.com>
2025-04-13 12:10:37 +0800

    去掉没有免费额度的gemini

--- app/constant.ts
+++ app/constant.ts
@@ -616,8 +616,8 @@ const googleModels = [
   // "gemini-2.0-pro-exp",
   // "gemini-2.0-pro-exp-02-05",
   "gemini-1.5-pro",
-  "gemini-2.0-flash",
-  "gemini-2.5-pro-preview-03-25",
+  "gemini-2.0-flash"
+  // "gemini-2.5-pro-preview-03-25",
 
 ];
 
